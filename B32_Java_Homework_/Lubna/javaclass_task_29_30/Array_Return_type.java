a0db5624325122b7b3aecb4d158b67cf4bdc5fa5
mojum <mojum@LAPTOP-C50CP82R.mynetworksettings.com>
2023-09-07 21:14:56 -0400

    Hi this is Lubna

---
+++ B32_Java_Homework_/Lubna/javaclass_task_29_30/Array_Return_type.java
@@ -0,0 +1,18 @@
+package javaclass_task_29_30;
+
+public class Array_Return_type {
+
+public static void main(String[] args) {
+	
+		//2) Create 4 Return type Methods .4 DIfferent Methods will return (1 dim Object Array,1 
+	//dim Int array,2 Dim String Array, 2 Dim Object Array). And Prove that it is returning 
+	//the Value ( By Printing the Calling method’s value , you can prove that But 
+		//remember , if it is returning an Array , You have to put the return value in an 
+	//Array box. And you can prove it by printing with for Each loops/. For loops) – 
+	//check next 2 example ss for reference
+	
+     }
+	
+		
+	}
+	
